60a157bda29ccba8015683984e256fe67eef3b08
Valentin Vlad <valentin_gabriel.vlad@yahoo.com>
2021-11-23 02:03:45 +0000

    adjust assertEqual to evaluate arrays and tail to returned the spliced original from index 1

---
+++ tail.js
@@ -0,0 +1,24 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let passed = "✅✅✅ Passed";
+  let failed = "🛑🛑🛑Failed";
+  let isEqual = true;
+  for (let i = 1; i < actual.length; i++) {
+    if (actual[i] !== expected[i])
+      isEqual = false;
+  }
+  
+  isEqual ? console.log(`Assertion ${passed}: [${actual}] === [${expected}]`) : console.log(`Assertion ${failed}: [${actual}] !== [${expected}]`);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+const tail = function(arr) {
+  return arr.slice(1);
+};
+
+assertEqual(tail([5,6,7]), [6,7]);
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+console.log(tail([]));
