10a5d48faf856fc9431a05ac39da30ab3fd359bb
Valentin Vlad <valentin_gabriel.vlad@yahoo.com>
2021-11-24 20:46:01 +0000

    add nice confirmation message of array equivalency

---
+++ assertArraysEqual.js
@@ -0,0 +1,39 @@
+
+const eqArrays = function(actual, expected) {
+  let eqArr = true;
+  let i = 0;
+  while (i < actual.length) {
+    if (actual[i] !== expected[i])
+      return false;
+    i++;
+  }
+  return eqArr;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  let passed = "Assertion ✅✅✅ Passed";
+  let failed = "Assertion 🛑🛑🛑 Failed";
+  let printAct = " First Array:";
+  let printExp = "Second Array:";
+  let isItEqual = eqArrays(actual, expected);
+  for (let i = 0; i < actual.length; i++) {
+    printAct += actual[i] + " " + typeof actual[i] + ", ";
+    printExp += expected[i] + " " + typeof expected[i] + ", ";
+  }
+  isItEqual ? console.log(`${passed}:\n${printAct}\n${printExp}`) : console.log(`${failed}:\n${printAct}\n${printExp}`);
+};
+
+// TEST CODE
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+// eqArrays([1, 2, 3], [1, 2, 3]) // => true
+// eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should fail
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => should PASS
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => should fail
