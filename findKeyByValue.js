bd0c4cc58448b7965d5cf3082766748c302a203a
Valentin Vlad <valentin_gabriel.vlad@yahoo.com>
2021-11-25 07:21:55 +0000

    add findByKey function

---
+++ findKeyByValue.js
@@ -0,0 +1,23 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let passed = "✅✅✅ Passed";
+  let failed = "🛑🛑🛑Failed";
+  actual === expected ? console.log(`Assertion ${passed}: [${actual}] === [${expected}]`) : console.log(`Assertion ${failed}: [${actual}] !== [${expected}]`);
+};
+
+const findKeyByValue = function(obj, val) {
+  for (key in obj) {
+    if (obj[key] === val)
+      return key;
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
