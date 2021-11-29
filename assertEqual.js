5334fc5af76aa7233ef2f525d869925b2d3a36e3
Valentin Vlad <valentin_gabriel.vlad@yahoo.com>
2021-11-29 16:23:42 -0500

    add require/export for head and assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   let passed = "✅✅✅ Passed";
-  let failed = "🛑🛑🛑Failed";
+  let failed = "🛑🛑🛑 Failed";
   actual === expected ? console.log(`Assertion ${passed}: [${actual}] === [${expected}]`) : console.log(`Assertion ${failed}: [${actual}] !== [${expected}]`);
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,18 +1,5 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  let passed = "✅✅✅ Passed";
-  let failed = "🛑🛑🛑Failed";
-  actual === expected ? console.log(`Assertion ${passed}: [${actual}] === [${expected}]`) : console.log(`Assertion ${failed}: [${actual}] !== [${expected}]`);
-};
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
 const head = function(arr) {
   return arr[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([],undefined));
+module.exports = head;
